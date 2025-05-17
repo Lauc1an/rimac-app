e2e3edbaba84a6b51984c2c44bee661e0d33953d
Lauc1an <marlomp@hotmail.com>
2025-05-17 15:49:24 -0500

    Add number mask to inputs

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import type { KeyboardEvent } from "react";
 import chevronIcon from "@/assets/images/chevron.svg";
 import type { DocumentListType } from "@/utils/Types";
 
@@ -20,10 +21,42 @@ function LoginForm() {
     toggleDropdown();
   };
 
+  const numberMask = (e: KeyboardEvent<HTMLInputElement>) => {
+    const key: string = e.key;
+    const isDigit: boolean = /[0-9]/.test(key);
+
+    const allowedKeys: string[] = [
+      "Backspace",
+      "Delete",
+      "Tab",
+      "Escape",
+      "Enter",
+      "ArrowLeft",
+      "ArrowRight",
+      "ArrowUp",
+      "ArrowDown",
+      "Home",
+      "End",
+      ".",
+    ];
+
+    const isCtrlCmdAction: boolean =
+      (e.ctrlKey || e.metaKey) &&
+      ["a", "c", "v", "x", "z"].includes(key.toLowerCase());
+
+    if (allowedKeys.includes(key) || isCtrlCmdAction || isDigit) return;
+
+    e.preventDefault();
+  };
+
   return (
     <form className="form">
       <div className="form__inputgroup">
-        <button type="button" className={dropdown ? "open" : ""} onClick={toggleDropdown}>
+        <button
+          type="button"
+          className={dropdown ? "open" : ""}
+          onClick={toggleDropdown}
+        >
           <p>{documentList[document]}</p>
           <img src={chevronIcon} alt="Chevron icon" />
         </button>
@@ -38,6 +71,9 @@ function LoginForm() {
             name="document"
             id="document"
             placeholder="Nro. de documento"
+            inputMode="numeric"
+            onKeyDown={numberMask}
+            maxLength={document === "dni" ? 8 : 9}
           />
           <label htmlFor="document">Nro. de documento</label>
         </div>
@@ -49,17 +85,15 @@ function LoginForm() {
           name="mobile"
           id="mobile"
           placeholder="Celular"
+          inputMode="numeric"
+          onKeyDown={numberMask}
+          maxLength={9}
         />
         <label htmlFor="mobile">Celular</label>
       </div>
 
       <div className="form__check">
-        <input
-          type="checkbox"
-          value="privacy"
-          name="privacy"
-          id="privacy"
-        />
+        <input type="checkbox" value="privacy" name="privacy" id="privacy" />
         <label htmlFor="privacy">Acepto la Política de Privacidad</label>
       </div>
 
@@ -75,7 +109,9 @@ function LoginForm() {
         </label>
       </div>
 
-      <a className="form__link" href="#">Aplican Términos y Condiciones</a>
+      <a className="form__link" href="#">
+        Aplican Términos y Condiciones
+      </a>
 
       <button type="submit">Cotiza aquí</button>
     </form>
