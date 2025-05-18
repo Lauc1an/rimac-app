be30d23ab74a274f5f2f80d52d8b7863045c88f1
Lauc1an <marlomp@hotmail.com>
2025-05-17 19:06:35 -0500

    Add auth types

--- src/utils/Types.ts
+++ src/utils/Types.ts
@@ -1,3 +1,35 @@
+import type { ReactNode } from "react";
+
 export interface DocumentListType {
   [key: string]: string
 };
+
+export interface LoginType {
+  type: string;
+  document: string;
+  mobile: string;
+};
+
+export interface UserType {
+  name:	string;
+  lastName:	string;
+  birthDay:	string;
+};
+
+export interface UserLoginType extends LoginType, UserType {}
+
+export interface PlansType {
+  name: string;
+  price: number;
+  description: Array<string>;
+  age: number;
+};
+
+export interface AuthContextType {
+  user: UserType | null;
+  setUser: (user: UserType | null) => void;
+};
+
+export interface ProviderProps {
+  children: ReactNode;
+}
